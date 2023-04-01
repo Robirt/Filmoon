e3d63c68b822ee4e2da3dcd7b24cc7b9a358a6a8
Robert Nancka <RobirtPL@outlook.com>
2023-04-01 13:56:09 +0200

    Add clean WPF project.

---
+++ Filmoon/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace Filmoon;
+
+public partial class App : Application
+{
+
+}
---
+++ Filmoon/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Filmoon;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
