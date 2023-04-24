781b901b1e97a6244759d05f7808535379f11b05
Robert Nancka <RobirtPL@outlook.com>
2023-04-24 21:06:37 +0200

    Some basics.

--- Filmoon/App.xaml.cs
+++ Filmoon/App.xaml.cs
@@ -1,8 +1,33 @@
-﻿using System.Windows;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace Filmoon;
 
 public partial class App : Application
 {
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration((context, builder) => { builder.AddJsonFile("appsettings.json", true, true); })
+            .ConfigureServices((context, services) => { services.AddRepositories().AddServices().AddViewModels().AddViews(); })
+            .Build();
+    }
 
+    private IHost AppHost { get; }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        await AppHost.StartAsync();
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+
+        await AppHost.StopAsync();
+    }
 }
---
+++ Filmoon/AppExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Filmoon;
+
+public static class AppExtensions
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddViewModels(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static IServiceCollection AddViews(this IServiceCollection services)
+    {
+        return services;
+    }
+}
