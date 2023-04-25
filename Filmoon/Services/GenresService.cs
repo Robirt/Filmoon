652b0eb1811395cd660bff63fe9d775f213b6596
martarozpondek <martarozpondek@wp.pl>
2023-04-25 22:21:41 +0200

    Create Commands, Services and View Models.

---
+++ Filmoon/Services/GenresService.cs
@@ -0,0 +1,21 @@
+﻿using Filmoon.Entities;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Filmoon.Services;
+
+public class GenresService
+{
+    public GenresService( HttpClient httpClient) 
+    {
+            HttpClient = httpClient;
+    }
+
+    private HttpClient HttpClient { get; }
+
+    public async Task AddGenreAsync(GenreEntity genre)
+    {
+        await HttpClient.PostAsJsonAsync<GenreEntity>("/Genres", genre);
+    }
+}
---
+++ Filmoon/ViewModels/Pages/SignInPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Filmoon.Commands;
+using Filmoon.Entities;
+using Filmoon.Requests;
+using Filmoon.Services;
+using System.Windows.Input;
+
+namespace Filmoon.ViewModels.Pages
+{
+    public class SignInPageViewModel: ViewModelBase
+    {
+        public SignInPageViewModel(UsersService usersService) 
+        {
+            UsersService = usersService;
+
+            SignInRequest = new UserSignInRequest();
+
+            SignInCommand = new SignInCommand(SignInAsync);
+        }
+        private UsersService UsersService { get; }
+
+        private UserSignInRequest signInRequest;
+
+        public UserSignInRequest SignInRequest
+        { 
+            get { return signInRequest; }
+            set{ SetProperty(ref signInRequest, value); }
+        }
+
+        public ICommand SignInCommand { get; set; }
+
+        private async void SignInAsync(object? parameter)
+        {
+            await UsersService.SignInAsync(parameter as UserSignInRequest);
+        }
+    }
+}
